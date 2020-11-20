dd3ed68d7f68788a55fe1e8603f5287c784842ec
secf4ult <secf4ult@outlook.com>
2020-11-20 21:50:31 +0800

    make sure mongodb has uri to connect

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const debug = require('debug')('app:database')
 
+if (!process.env.MONGODB_URI) require('dotenv').config()
+
 const mongodbOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true
