3f10d6bfd61c93dc9a7bdc47962222bd7a1b7a06
secf4ult <secf4ult@outlook.com>
2024-08-05 11:50:43 +0900

    Initial Commit

---
+++ SquirrelledTests/SquirrelledTests.swift
@@ -0,0 +1,17 @@
+//
+//  SquirrelledTests.swift
+//  SquirrelledTests
+//
+//  Created by Yu Xiao on 8/5/24.
+//
+
+import Testing
+@testable import Squirrelled
+
+struct SquirrelledTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
